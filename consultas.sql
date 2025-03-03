47acda83dc72e8898d5b5e5febc640a6a38aeab6
Estefanía Hernández <161893097+HEstefaniaR@users.noreply.github.com>
2025-03-03 12:51:29 -0500

    Entrega 1

--- consultas.sql
+++ consultas.sql
@@ -1,4 +1,4 @@
--- 1. ¿Qué usuarios han realizado más de 2 solicitudes y han recibido una calificación menor a 3?
+-- Personas con mas de 2 solicitudes y una calificacion menor a 3
 SELECT  U.nombreCompleto, COUNT(S.idsolicitud) AS totalSolHechas, 
 MIN(C.puntuacion) AS peorCalificacion
 FROM USUARIO AS U
@@ -8,7 +8,7 @@ JOIN CALIFICACION C ON R.idrespuesta = C.idrespuesta
 GROUP BY U.idusuario, U.nombreCompleto
 HAVING COUNT(S.idsolicitud) > 2 AND MIN(C.puntuacion) IN ('1', '2');
 
--- 2. ¿Qué administradores han respondido más solicitudes que el promedio?
+-- Administradores que han respondido mas solicitudes que el promedio
 SELECT A.idadmin, A.usuario AS usuarioAdmin, COUNT(R.idrespuesta) AS totalRespuestas
 FROM ADMIN AS A
 JOIN RESPUESTA AS R ON A.idadmin = R.idadmin
@@ -17,15 +17,14 @@ HAVING totalRespuestas > (SELECT AVG(totalResp)
 			FROM (SELECT COUNT(R.idrespuesta) AS totalResp
 					FROM RESPUESTA AS R GROUP BY R.idadmin) AS promRespuestas);
 
--- 3. ¿Cuál es el promedio general de calificaciones y el promedio de respuestas por solicitud?
-SELECT 
-    (SELECT ROUND(AVG(puntuacion), 2) FROM calificacion) AS promedio_general_calificaciones,
+-- Promedio general de calificaciones y promedio de respuestas por solicitud
+SELECT (SELECT ROUND(AVG(puntuacion), 2) FROM calificacion) AS promedio_general_calificaciones,
     (SELECT ROUND(AVG(conteo), 2) 
      FROM (SELECT COUNT(*) AS conteo 
            FROM respuesta 
            GROUP BY idsolicitud) AS temp) AS promedio_respuestas_por_solicitud;
 
--- 4. ¿Cuáles son las solicitudes que tienen más respuestas que el promedio del sistema?
+-- Solicitudes con mas respuesta que el promedio
 SELECT 
     s.idsolicitud,
     s.tipo,
@@ -39,7 +38,7 @@ GROUP BY s.idsolicitud
 HAVING COUNT(r.idrespuesta) > @promedio
 ORDER BY num_respuestas DESC;
 
--- 5. ¿Cuáles son los clientes con más quejas en el último mes?
+-- Clientes con mayor cantidad de quejas en el ultimo mes
 SELECT u.nombreCompleto AS cliente, COUNT(*) AS total_quejas
 FROM solicitud s
 JOIN usuario u ON s.idusuario = u.idusuario
@@ -48,7 +47,7 @@ AND s.fechaHoraCreacion >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH) -- Último mes
 GROUP BY u.idusuario, u.nombreCompleto
 ORDER BY total_quejas DESC;
 
--- 6. ¿Qué clientes han registrado más reclamos en el sistema?
+-- Clientes con mayor cantidad de reclamos 
 SELECT u.nombreCompleto AS cliente, COUNT(s.idsolicitud) AS total_quejas
 FROM solicitud s
 JOIN usuario u ON s.idusuario = u.idusuario
@@ -56,7 +55,7 @@ WHERE s.tipo = 'reclamo'
 GROUP BY u.idusuario, u.nombreCompleto
 ORDER BY total_quejas DESC;
 
--- 7. ¿Cuál es el tiempo promedio, mínimo y máximo de resolución de solicitudes con evidencia?
+-- Tiempo promedio, minimo y maximo de las solicitudes con evidencia resueltas
 SELECT tipo, 
        AVG(TIMESTAMPDIFF(DAY, fechaHoraCreacion, fechaActualizacion)) AS tiempo_promedio_dias,
        MIN(TIMESTAMPDIFF(DAY, fechaHoraCreacion, fechaActualizacion)) AS tiempo_minimo_dias,
@@ -67,7 +66,7 @@ WHERE estado = 'resuelta'
 GROUP BY tipo
 ORDER BY tiempo_promedio_dias DESC;
 
--- 8. ¿Qué administradores tienen más respuestas pendientes de contestar?
+-- Administradores con más respuestas sin contestar
 SELECT a.usuario, COUNT(r.idRespuesta) AS respuestas_pendientes
 FROM admin a
 JOIN respuesta r ON a.idAdmin = r.idAdmin
@@ -75,23 +74,19 @@ WHERE r.comentario IS NULL OR r.comentario = ''
 GROUP BY a.idAdmin
 ORDER BY respuestas_pendientes DESC;
 
--- 9. ¿Cuáles son los PQRS con la mayor cantidad de evidencias adjuntas?
-SELECT 
-    u.idusuario AS S.idsolicitud, 
-     s.descripcion,  
-    COUNT(e.id) AS total_evidencias
-FROM Solicitud s
+-- Usuarios que han subido mas de 2 evidencias
+SELECT u.idusuario, u.nombreCompleto, COUNT(e.idevidencia) AS total_evidencias
+FROM Usuario u
+JOIN Solicitud s ON u.idusuario = s.idusuario
 JOIN Evidencia e ON s.idsolicitud = e.idsolicitud
 GROUP BY u.idusuario
-ORDER BY total_evidencias DESC 
-LIMIT 20; 
+HAVING total_evidencias > 3
+ORDER BY total_evidencias DESC;
 
--- 10. ¿Cuales PQRS no han recibido respuesta en más de 15 días?
-SELECT 
-    u.idusuario AS S.idsolicitud, 
-     s.descripcion, 
-    s.fechaHoraCreacion
+-- Calificacion promedio de respuesta por tipo de solicitud
+SELECT s.tipo, ROUND(AVG(c.puntuacion),2) AS calificacion_promedio
 FROM Solicitud s
-WHERE estado = 'resuelta' 
-AND DATEDIFF(NOW(), s.fechaHoraCreacion) > 15;
-
+JOIN Respuesta r ON s.idsolicitud = r.idsolicitud
+JOIN Calificacion c ON r.idrespuesta = c.idrespuesta
+GROUP BY s.tipo
+ORDER BY calificacion_promedio DESC;
